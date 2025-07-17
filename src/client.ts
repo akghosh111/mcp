c8efbdca1269134e0cba1d8264ed6fd49e9e36b3
akghosh111 <ghoshkiran2017@gmail.com>
2025-07-17 17:12:19 +0530

    add client mcp call

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,8 @@ import "dotenv/config"
 import { Client } from "@modelcontextprotocol/sdk/client/index.js"
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js"
 import select from "@inquirer/select";
+import input from "@inquirer/input";
+import { Tool } from "@modelcontextprotocol/sdk/types.js";
 
 const mcp = new Client({
     name: "text-client-video",
@@ -47,9 +49,33 @@ async function main() {
                         description: tool.description,
                     })),
                 })
-                console.log(toolName)
+                const tool = tools.find(t => t.name === toolName);
+                if (!tool) {      
+                    console.error("Tool not found");
+                    
+                } else {
+                    await handleTool(tool);
+                }
+                break
         }
     }
+}
+
+async function handleTool(tool: Tool) {
+    const args : Record<string, string> = {};
+
+    for (const [key, value] of Object.entries(tool.inputSchema.properties ?? {})) {
+        args[key] = await input({
+            message: `Enter value for ${key} (${(value as { type: string }).type}):`,
+
+        });
+    }
+    const res = await mcp.callTool({
+        name: tool.name,
+        arguments: args
+    })
+
+    console.log((res.content as [{ text: string }])[0].text);
 }
 
 
